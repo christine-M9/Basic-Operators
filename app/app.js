526caae84d553a17e8ea72b7e1d7229b7df2c1d1
Christine <christinekhekama99@gmail.com>
2024-06-01 22:58:35 +0300

    Basic operators udemy

---
+++ app/app.js
@@ -0,0 +1,24 @@
+// question 1
+let population1 = 46;
+// let population2 = 46 / 2;
+// console.log(population2);
+console.log(population1 / 2);
+
+// question 2
+population1++;
+console.log(population1);
+
+// question 3
+let country1 = "Kenya";
+Kenya = 47;
+let country2 = "Finland";
+Finland = 6;
+console.log(country1 > country2);
+
+// question 4
+let averagePopulation = 33;
+console.log(country1 < averagePopulation);
+
+let description = country1 + " " + "is in Africa and its" + " " + population1 + " " + "million people speak English";
+console.log(description);
+
